3250cfdb61f71dbda03b6213b4a7abaa6fe84859
Ireneusz Niedbala <ireknie00@gmail.com>
2025-03-22 17:27:19 +1100

    remove failed config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/sap-exam-prep2/', // Update this to match your deployment subpath
-  build: {
-    outDir: 'dist', // Ensure this matches your output directory
-  },
-});
-
